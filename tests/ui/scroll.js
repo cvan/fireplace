49c6084c960ada5e85ba4ffc831a90a6de07a08b
Christopher Van <cvan@mozilla.com>
2014-11-14 15:47:30 -0800

    reset the scroll position immediately when not on Android (bug 986625)

--- tests/ui/scroll.js
+++ tests/ui/scroll.js
@@ -1,49 +1,100 @@
 var helpers = require('../helpers');
 
 var firstItemSel = '#gallery .listing li:first-child a';
+var originalFirefoxAndroid;
 var scrollPos;
 
 helpers.startCasper({path: '/category/games'});
 
+function getScrollY() {
+    return casper.evaluate(function() {
+        return window.scrollY;
+    });
+}
+
+function scrollToElementAndGetScrollY(selector) {
+    return casper.evaluate(function(selector) {
+        document.querySelector(selector).scrollIntoView();
+        return window.scrollY;
+    }, selector);
+}
+
+function mockFirefoxAndroid(enabled, original) {
+    casper.echo((original ? 'Restoring' : 'Settings') +
+        ' capabilities.firefoxAndroid -> ' + enabled, 'INFO');
+    return casper.evaluate(function(enabled) {
+        return require('capabilities').firefoxAndroid = enabled;
+    }, enabled);
+}
+
 casper.test.begin('General Scroll tests', {
+    setUp: function() {
+        casper.once('page.initialized', function() {
+            originalFirefoxAndroid = casper.evaluate(function() {
+                return require('capabilities').firefoxAndroid;
+            });
+            casper.echo('Remembering capabilities.firefoxAndroid <- ' +
+                originalFirefoxAndroid, 'INFO');
+        });
+    },
+
+    tearDown: function() {
+        mockFirefoxAndroid(originalFirefoxAndroid, true);
+    },
 
     test: function(test) {
-
         casper.waitForSelector('#gallery .listing', function() {
-            scrollPos = casper.evaluate(function(Y) {
-                window.scrollTo(0, Y);
-                return window.scrollY;
-            }, casper.getElementBounds(firstItemSel).top);
+            var startScrollY = getScrollY();
+            test.assertEquals(startScrollY, 0, 'ScrollY starts at 0');
+
+            scrollPos = scrollToElementAndGetScrollY(firstItemSel);
             casper.click(firstItemSel);
-        });
 
-        casper.wait(300, function() {
-            var scrollY = casper.evaluate(function() {
-                return window.scrollY;
+            mockFirefoxAndroid(true);
+
+            casper.wait(300, function() {
+                var scrollY = getScrollY();
+                test.assertEquals(scrollY, 0, 'Check scroll is 0');
+                casper.back();
             });
-            test.assertEquals(scrollY, 0, 'Check scroll is 0');
-            casper.back();
-        });
 
-        casper.wait(300, function() {
-            var scrollY = casper.evaluate(function() {
-                return window.scrollY;
+            casper.wait(300, function() {
+                var scrollY = getScrollY();
+                test.assert(scrollY > 0, 'Check scroll is greater than 0');
+                test.assertEquals(scrollY, scrollPos, "Check scroll hasn't changed");
+                casper.click(firstItemSel);
             });
-            test.assert(scrollY > 0, 'Check scroll is greater than 0');
-            test.assertEquals(scrollY, scrollPos, "Check scroll hasn't changed");
-            casper.click(firstItemSel);
-        });
 
-        casper.waitForSelector('#nav-back', function() {
-            casper.click('#nav-back');
-        });
-
-        casper.wait(300, function() {
-            var scrollY = casper.evaluate(function() {
-                return window.scrollY;
+            casper.waitForSelector('#nav-back', function() {
+                casper.click('#nav-back');
             });
-            test.assert(scrollY > 0, 'Check scroll is greater than 0');
-            test.assertEquals(scrollY, scrollPos, "Check scroll hasn't changed");
+
+            casper.wait(300, function() {
+                var scrollY = getScrollY();
+                test.assert(scrollY > 0, 'Check scroll is greater than 0');
+                test.assertEquals(scrollY, scrollPos, "Check scroll hasn't changed");
+
+                casper.click('.site a');  // Navigate to homepage.
+            });
+
+            // Wait for homepage to load.
+            casper.waitForSelector('.navbar', function() {
+                var startScrollY = getScrollY();
+                test.assertEquals(startScrollY, 0, 'ScrollY starts at 0');
+
+                scrollPos = scrollToElementAndGetScrollY('.navbar');
+                casper.click('.navbar a');
+
+                mockFirefoxAndroid(false);
+
+                var scrollY = getScrollY();
+                console.log('scrollY', scrollY);
+                test.assertEquals(scrollY, 0, 'Check scroll is 0');
+                casper.back();
+
+                scrollY = getScrollY();
+                test.assertEquals(scrollY, 0, 'Check scroll is 0');
+            });
         });
 
         casper.run(function() {
